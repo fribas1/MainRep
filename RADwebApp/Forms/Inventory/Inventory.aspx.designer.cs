2b187f3641079f392f65c9aa8771425ec2154552
Filip Lyesovyy <lyesovyyf@gmail.com>
2018-12-11 17:08:39 -0500

    inventory

--- RADwebApp/Forms/Inventory/Inventory.aspx.designer.cs
+++ RADwebApp/Forms/Inventory/Inventory.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace RADwebApp.Forms.Inventory {
         protected global::System.Web.UI.WebControls.Button btnLogout;
         
         /// <summary>
-        /// panelFilter control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel panelFilter;
-        
-        /// <summary>
         /// ddlBrands control.
         /// </summary>
         /// <remarks>
